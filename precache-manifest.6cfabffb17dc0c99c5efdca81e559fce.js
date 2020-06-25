4093af04149ba047f16df2e870d095121b701bcf
Gianluca Maccioni <gianluca.maccioni.uk@gmail.com>
2020-06-25 22:12:07 +0100

    Updates

---
+++ precache-manifest.6cfabffb17dc0c99c5efdca81e559fce.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "9b1c4eb9acc313f5a4dd6534cbb3a134",
+    "url": "/FCC-Pomodoro-Clock-React/index.html"
+  },
+  {
+    "revision": "2f9049663700a1678693",
+    "url": "/FCC-Pomodoro-Clock-React/static/css/main.30f6d12f.chunk.css"
+  },
+  {
+    "revision": "3232172c1aac25e1c737",
+    "url": "/FCC-Pomodoro-Clock-React/static/js/2.1bb269de.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/FCC-Pomodoro-Clock-React/static/js/2.1bb269de.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "2f9049663700a1678693",
+    "url": "/FCC-Pomodoro-Clock-React/static/js/main.91d70718.chunk.js"
+  },
+  {
+    "revision": "4af5bb9190f105c9c289",
+    "url": "/FCC-Pomodoro-Clock-React/static/js/runtime-main.d960b800.js"
+  }
+]);
